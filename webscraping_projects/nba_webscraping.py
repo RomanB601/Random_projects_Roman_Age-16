514c55fe6d85b2346db0d5eb76dc642c6e604c8b
RomanB601 <bonfiglioroman@gmail.com>
2022-12-24 17:21:45 -0500

    improved nba script

--- webscraping_projects/nba_webscraping.py
+++ webscraping_projects/nba_webscraping.py
@@ -4,14 +4,15 @@ import requests
 
 
 def main():
-    print("What NBA Player do you want stats on?")
-    player = str(input(">"))
-    player_new,let = player_algo(player) #makes the input the html requirement
+    print("\nWhat NBA Player do you want stats on?")
+    player_new,let,realplayer = player_algo() #makes the input the html requirement
     html_text = requests.get(f"https://www.basketball-reference.com/players/{let}/{player_new}.html").text
     soup = BeautifulSoup(html_text, "html5lib")
     stats = soup.find_all("div", class_="stats_pullout")
+    nicknames = soup.find_all("div", id = "meta")
+    socials, nicknames = get_nicknames(nicknames) #gets the nick names and socials of the player
     stats1 , stats2 = get_stats(stats) # assigns stats1 as the dictorary for stats1 and stats2 as the dict for stats2
-    print(split_dict_print(stats1, stats2, player)) #prints everything out
+    print(split_dict_print(stats1, stats2, realplayer,socials,nicknames)) #prints everything out
 
 def get_stats(stats_html): #gets the stats using Beautiful Soup
     my_dict1 = {} # Makes a dict to store p1 of the stats and part 2 for dict2 
@@ -32,31 +33,44 @@ def get_stats(stats_html): #gets the stats using Beautiful Soup
             my_dict2["FGT%"] = p2[5].text.strip()
     return my_dict1, my_dict2
 
-def split_dict_print(dict1, dict2,player): # prints out the stats, basically a to string
-    print (f'\n{player} has had {dict1["Games"]} Games, a average of {dict1["Pts"]} Points per Game, {dict1["TRB"]} Rebounds per Game, {dict1["AST"]} Assists per Game')
-    return f'{player} has had a {dict2["FG%"]} Shot%, {dict2["FG3%"]} 3-Point %, {dict2["FGT%"]} FreeThrow%'
+def get_nicknames(text_html): #gets the nicknames and socials of the player
+    my_dict = {}
+    for names in text_html:
+        names = names.find_all("p")
+        for index,name in enumerate(names):
+            my_dict[index] = name.text.strip()
+    return my_dict[0].replace("\n",""),my_dict[1].replace("(","").replace(")","") #returns the socials and nicknames and gets rid of uneed things
 
-def player_algo(player): # makes the player name the link required name and gets the first letter of the last name
-    try:
-        if player.replace(" ","") != player:
-            first, last = player.split(" ")
-            letter = last[0]
-            if len(last) >= 6: # if last name is smaller than 6 than don't get rid of letters
-                newlast = last[:len(last)-1] + last[len(last):]
-                newfirst = first[:2]
-                newname = newlast + newfirst + "01"
-                return newname, letter
+def split_dict_print(dict1, dict2,player,socials, nicknames): # prints out the stats, basically a to string
+    print(f'\n{socials}\n{player} nicknames are |{nicknames}|')
+    print (f'\n{player} has had {dict1["Games"]} Games, a average of {dict1["Pts"]} Points per Game, {dict1["TRB"]} Rebounds per Game, {dict1["AST"]} Assists per Game')
+    return f'{player} has had a {dict2["FG%"]} Shot%, {dict2["FG3%"]} 3-Point %, {dict2["FGT%"]} FreeThrow%\n'
+
+def player_algo(): # makes the player name the link required name and gets the first letter of the last name
+    while True:
+        try:
+            player = str(input(">"))
+            if player.replace(" ","") != player:
+                first, last = player.split(" ")
+                letter = last[0]
+                if len(last) >= 6: # if last name is smaller than 6 than don't get rid of letters
+                    newlast = last[:len(last)-1] + last[len(last):]
+                    newfirst = first[:2]
+                    newname = newlast + newfirst + "01"
+                    return newname, letter,player
+                    break
+                else:
+                    newfirst = first[:2]
+                    newname = last + newfirst + "01"
+                    return newname, letter,player
+                    break
             else:
-                newfirst = first[:2]
-                newname = last + newfirst + "01"
-                return newname, letter
-        else:
-            raise TypeError
-    except ValueError:
-        print("Enter a Real NBA Player\n")
-        main()
-    except TypeError:
-        print("Please put spaces between the first and last name\n")
-        main()
+                raise TypeError
+        except ValueError:
+            print("Enter a Real NBA Player\n")
+        except TypeError:
+            print("Please put spaces between the first and last name\n")
+
+
 if __name__ == "__main__":
     main()
